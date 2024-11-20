92f1c4c0dc930f05e0f1c4ce6e8f11cc59a8b8e8
atroian <andrii.troian@corezoid.com>
2024-11-20 14:27:41 -0500

    initial commit

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,18 +1,15 @@
 import jwt from 'jsonwebtoken';
 
 export const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    // The token is usually sent as 'Bearer <token>'
-    const token = authHeader && authHeader.split(' ')[1];
+    const token = req.cookies.token;
 
     if (!token) {
         return res.status(401).json({ message: 'Access token missing or invalid' });
     }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(403).json({ message: 'Invalid or expired token' });
-        }
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+        if (err) return res.status(403).json({ message: 'Invalid or expired token' });
+
         // Attach the user ID to the request object
         req.userId = decoded.userId;
         next();
